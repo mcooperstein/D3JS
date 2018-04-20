d77bf79c8bd96f7375239ab6c2ccf2ab63322c74
Marc Cooperstein <mcooperstein@gmail.com>
2018-04-19 17:59:53 -0700

    pie chart

--- app.js
+++ app.js
@@ -154,11 +154,11 @@ svg.append('g')
 */
 
 // SVG Shapes
-
+/*
 var svgWidth = 500;
 var svgHeight = 400;
 
-var svg = d3.select('#elements')
+var svg = d3.select('#shapes')
     .attr('width', svgWidth)
     .attr('height', svgHeight)
 
@@ -195,3 +195,70 @@ var circle2 = svg.append('circle')
     .attr('cy', 140)
     .attr('r', 40)
     .attr('fill', 'white')
+*/
+
+// Pie Charts
+
+var data = [
+    {
+        "result": "Win",
+        "percentage": 54.8
+    },
+    {
+        "result": "Loss",
+        "percentage": 33
+    },
+    {
+        "result": "OT Loss",
+        "percentage": 12.2
+    }
+];
+
+var svgWidth = 500;
+var svgHeight = 300;
+var radius = Math.min(svgWidth, svgHeight) / 2;
+
+var svg = d3.select('.pie-chart')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
+
+
+// create group element to hold pie chart
+/*var g = svg.append('g')
+    .attr('transform', `translate${radius},${radius}`);*/
+var g = svg.append('g')
+    .attr('transform', `translate(${svgWidth/2},${svgHeight/2})`);
+
+
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var pie = d3.pie().value(function (d) {
+    return d.percentage;
+})
+
+var path = d3.arc().outerRadius(radius).innerRadius(0);
+
+var arc = g.selectAll('arc')
+    .data(pie(data))
+    .enter()
+    .append('g')
+
+arc.append('path')
+    .attr('d', path)
+    .attr('fill', function (d) {
+        return color(d.data.percentage)
+    });
+
+var label = d3.arc()
+    .outerRadius(radius)
+    .innerRadius(0)
+
+arc.append('text')
+    .attr('transform', function (d) {
+        return `translate(${label.centroid(d)})`
+    })
+    .attr('text-anchor', 'middle')
+    .attr('font-weight', 'bold')
+    .text(function (d) {
+        return d.data.result + ": " + d.data.percentage + "%"
+    });
