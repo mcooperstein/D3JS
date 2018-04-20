a6f8b89ffeda9de0869a9220b82da8d805d43b21
Marc Cooperstein <mcooperstein@gmail.com>
2018-04-19 18:34:49 -0700

    added bitcoin line chart using data from coindesk API

--- app.js
+++ app.js
@@ -230,7 +230,7 @@ var g = svg.append('g')
     .attr('transform', `translate(${svgWidth/2},${svgHeight/2})`);
 
 
-var color = d3.scaleOrdinal(d3.schemeCategory10);
+var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var pie = d3.pie().value(function (d) {
     return d.percentage;
@@ -262,3 +262,121 @@ arc.append('text')
     .text(function (d) {
         return d.data.result + ": " + d.data.percentage + "%"
     });
+
+
+// Line Chart
+
+//API to fetch historical data of Bitcoin Price Index
+
+function displayDate() {
+    const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+    var currentMonth = new Date().getMonth();
+    var currentYear = new Date().getFullYear();
+    var month = months[currentMonth];
+    var dateNum = new Date().getDate();
+    return `${currentYear}-${month}-${dateNum}`;
+}
+
+let today = displayDate()
+
+const api = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2012-01-01&end=' + today;
+
+/**
+ * Loading data from API when DOM Content has been loaded'.
+ */
+document.addEventListener("DOMContentLoaded", function (event) {
+    fetch(api)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var parsedData = parseData(data);
+            drawChart(parsedData);
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+});
+
+/**
+ * Parse data into key-value pairs
+ * @param {object} data Object containing historical data of BPI
+ */
+function parseData(data) {
+    var arr = [];
+    for (var i in data.bpi) {
+        arr.push({
+            date: new Date(i), //date
+            value: +data.bpi[i] //convert string to number
+        });
+    }
+    return arr;
+}
+
+/**
+ * Creates a chart using D3
+ * @param {object} data Object containing historical data of BPI
+ */
+function drawChart(data) {
+    var svgWidth = 600,
+        svgHeight = 400;
+    var margin = {
+        top: 20,
+        right: 20,
+        bottom: 30,
+        left: 50
+    };
+    var width = svgWidth - margin.left - margin.right;
+    var height = svgHeight - margin.top - margin.bottom;
+
+    var svg = d3.select('.line-chart')
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+
+    var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scaleTime()
+        .rangeRound([0, width]);
+
+    var y = d3.scaleLinear()
+        .rangeRound([height, 0]);
+
+    var line = d3.line()
+        .x(function (d) {
+            return x(d.date)
+        })
+        .y(function (d) {
+            return y(d.value)
+        })
+    x.domain(d3.extent(data, function (d) {
+        return d.date
+    }));
+    y.domain(d3.extent(data, function (d) {
+        return d.value
+    }));
+
+    g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .select(".domain")
+
+    g.append("g")
+        .call(d3.axisLeft(y))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Price ($)");
+
+    g.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
+}
